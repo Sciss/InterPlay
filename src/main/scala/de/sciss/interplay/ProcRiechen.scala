207077b5cf4170d0dfd0d5c6d2758127f6e01d48
Sciss <contact@sciss.de>
2011-02-06 18:07:42 +0000

    giving up with replaceTailChain now, putting everything in a single proc. seems fine

--- src/main/scala/de/sciss/interplay/ProcRiechen.scala
+++ src/main/scala/de/sciss/interplay/ProcRiechen.scala
@@ -72,81 +72,113 @@ object ProcRiechen extends Process {
    val MIN_FADEOUT         = 1.0
    val MAX_FADEOUT         = 20.0
 
-   private val catchName   = name + "-catch"
-   private val throwName   = name + "-throw"
+//   private val catchName   = name + "-catch"
+//   private val throwName   = name + "-throw"
 //   private val freezeName  = name + "-freeze"
-   private val hilbName    = name + "-hilb"
-   private lazy val urn    = new Urn( hilbName )
+//   private val hilbName    = name + "-hilb"
+//   private lazy val urn    = new Urn( hilbName )
 
-//   private val fltProcRef  = Ref( Set.empty[ Proc ])
-   private val orgRef      = Ref( Option.empty[ Org ])
-   case class Org( catchP: Proc, throwP: Proc, fltP: ISet[ Proc ])
+//   private val orgRef      = Ref( Option.empty[ Org ])
+//   case class Org( catchP: Proc, throwP: Proc, fltP: ISet[ Proc ])
 
    def init(  implicit tx: ProcTxn ) {
-      diff( catchName ) {
-         val pouts = Seq.tabulate( MASTER_NUMCHANNELS )( i => pAudioOut( "out" + i ))
+//      diff( catchName ) {
+//         val pouts = Seq.tabulate( MASTER_NUMCHANNELS )( i => pAudioOut( "out" + i ))
+//         graph { in =>
+//            require( in.numOutputs == MASTER_NUMCHANNELS, "Just got " + in.numOutputs + " chans" )
+//            pouts.zip( in.outputs ).foreach { tup =>
+//               val (pout, sig) = tup
+//               pout.ar( sig ) // splittendorfer
+//            }
+//            0.0
+//         }
+//      }
+//
+//      gen( throwName ) {
+//         val pins = Seq.tabulate( MASTER_NUMCHANNELS )( i => pAudioIn( "in" + i ))
+//         val pamp  = pAudio( "amp", ParamSpec( 0, 10 ), 1 )
+//         graph {
+//            val in: GE = pins.map( _.ar ) // klebendorfer
+//            require( in.numOutputs == MASTER_NUMCHANNELS )
+//            in * pamp.ar
+//         }
+//      }
+//
+//      def mkDurMix() = pScalar( "dur", ParamSpec( 1, 60 ), 10 ) -> pAudio( "mix", ParamSpec( 0, 1 ), 1 )
+//      def mix( in: GE, flt: GE, pDurMix: (ProcParamScalar, ProcParamAudio) ) : GE = {
+//         val (pdur, pmix) = pDurMix
+//         val me = Proc.local
+//         Done.kr( Line.kr( 0, 0, pdur.ir )).react {
+//            spawnAtomic( name + " mix done" ) { implicit tx =>
+//// unfortunately currently no way to determine end of this xfade...
+////             xfade( ... )( me.bypass )
+////               ProcessHelper.whenFadeDone()
+//               glide( exprand( MIN_FADEOUT, MAX_FADEOUT ))( me.control( "mix" ).v = 0 )
+//               ProcessHelper.whenGlideDone( name + " mix done", me, "mix" ) { implicit tx =>
+//                  orgRef() match {
+//                     case Some( orgOld ) =>
+//                        val orgNew = orgOld.copy( fltP = orgOld.fltP - me )
+//                        if( orgNew.fltP.isEmpty ) {  // this was the last one
+//                           orgRef.set( None )
+//                           removeAndDisposeChain( name + " mix all done", orgNew.catchP, orgNew.throwP )
+//                           reentry()
+//                        } else {
+//                           orgRef.set( Some( orgNew ))
+//                        }
+//                     case None => informDir( "Weird - no org found?!", force = true )
+//                  }
+//               }
+//            }
+//         }
+//         XFade2.ar( in, flt, pmix.ar * 2 - 1 )
+//      }
+//
+//      filter( hilbName ) {
+//         val pin2 = pAudioIn( "in2" )
+//         val dm   = mkDurMix()
+//         graph { in1 =>
+//            val in2  = pin2.ar
+//            val norm = Normalizer.ar( in2, dur = 0.02 )
+////            val norm = in2 * 2 * PeakFollower.ar( in2, 2 ).max( 0.01 ).reciprocal   // hmmm....
+////            val rms  = LagUD.ar( in2.squared.squared, 0.1, 1.0 )
+////            val norm = in2 * rms.max( 0.01 ).reciprocal
+//            val Seq(re1, im1) = Hilbert.ar( in1 ).outputs
+//            val Seq(re2, im2) = Hilbert.ar( norm ).outputs
+//            val flt  = re1 * re2 - im1 * im2
+//            mix( in1, flt, dm )
+//         }
+//      }
+
+      filter( name ) {
          graph { in =>
-            require( in.numOutputs == MASTER_NUMCHANNELS, "Just got " + in.numOutputs + " chans" )
-            pouts.zip( in.outputs ).foreach { tup =>
-               val (pout, sig) = tup
-               pout.ar( sig ) // splittendorfer
-            }
-            0.0
-         }
-      }
-
-      gen( throwName ) {
-         val pins = Seq.tabulate( MASTER_NUMCHANNELS )( i => pAudioIn( "in" + i ))
-         val pamp  = pAudio( "amp", ParamSpec( 0, 10 ), 1 )
-         graph {
-            val in: GE = pins.map( _.ar ) // klebendorfer
             require( in.numOutputs == MASTER_NUMCHANNELS )
-            in * pamp.ar
-         }
-      }
 
-      def mkDurMix() = pScalar( "dur", ParamSpec( 1, 60 ), 10 ) -> pAudio( "mix", ParamSpec( 0, 1 ), 1 )
-      def mix( in: GE, flt: GE, pDurMix: (ProcParamScalar, ProcParamAudio) ) : GE = {
-         val (pdur, pmix) = pDurMix
-         val me = Proc.local
-         Done.kr( Line.kr( 0, 0, pdur.ir )).react {
-            spawnAtomic( name + " mix done" ) { implicit tx =>
-// unfortunately currently no way to determine end of this xfade...
-//             xfade( ... )( me.bypass )
-//               ProcessHelper.whenFadeDone()
-               glide( exprand( MIN_FADEOUT, MAX_FADEOUT ))( me.control( "mix" ).v = 0 )
-               ProcessHelper.whenGlideDone( name + " mix done", me, "mix" ) { implicit tx =>
-                  orgRef() match {
-                     case Some( orgOld ) =>
-                        val orgNew = orgOld.copy( fltP = orgOld.fltP - me )
-                        if( orgNew.fltP.isEmpty ) {  // this was the last one
-                           orgRef.set( None )
-                           removeAndDisposeChain( name + " mix all done", orgNew.catchP, orgNew.throwP )
-                           reentry()
-                        } else {
-                           orgRef.set( Some( orgNew ))
-                        }
-                     case None => informDir( "Weird - no org found?!", force = true )
-                  }
+            val modChans: IIdxSeq[ Int ] = scramble2( 0 until MASTER_NUMCHANNELS )
+            val coll = List.tabulate( MASTER_NUMCHANNELS ) { ch =>
+               val in1  = in \ ch
+//               val in2  = Select.ar( modChans( ch ) ... hmmmm, this needs to be a control to make sense )
+               val in2  = in \ modChans( ch )
+               val norm = Normalizer.ar( in2, dur = 0.02 )
+               val Seq(re1, im1) = Hilbert.ar( in1 ).outputs
+               val Seq(re2, im2) = Hilbert.ar( norm ).outputs
+               val flt     = re1 * re2 - im1 * im2
+               val fadeIn  = ExpRand( MIN_FADEIN, MAX_FADEIN )
+               val fadeOut = ExpRand( MIN_FADEOUT, MAX_FADEOUT )
+               val dur     = ExpRand( MIN_DUR, MAX_DUR )
+               val mix     = EnvGen.ar( Env.linen( fadeIn, dur - (fadeIn + fadeOut), fadeOut ))
+               val done    = Done.kr( mix )
+               XFade2.ar( in1, flt, mix * 2 - 1 ) -> done
+            }
+            val pflt: GE = coll.map( _._1 )
+            val done: GE = coll.map( _._2 )
+            val me = Proc.local
+            (Mix( done ) > (MASTER_NUMCHANNELS - 0.5)).react {
+               spawnAtomic( name + " filter done" ) { implicit tx =>
+                  ProcessHelper.stopAndDispose( me )
+                  reentry()
                }
             }
-         }
-         XFade2.ar( in, flt, pmix.ar * 2 - 1 )
-      }
-
-      filter( hilbName ) {
-         val pin2 = pAudioIn( "in2" )
-         val dm   = mkDurMix()
-         graph { in1 =>
-            val in2  = pin2.ar
-            val norm = Normalizer.ar( in2, dur = 0.02 )
-//            val norm = in2 * 2 * PeakFollower.ar( in2, 2 ).max( 0.01 ).reciprocal   // hmmm....
-//            val rms  = LagUD.ar( in2.squared.squared, 0.1, 1.0 )
-//            val norm = in2 * rms.max( 0.01 ).reciprocal
-            val Seq(re1, im1) = Hilbert.ar( in1 ).outputs
-            val Seq(re2, im2) = Hilbert.ar( norm ).outputs
-            val flt  = re1 * re2 - im1 * im2
-            mix( in1, flt, dm )
+            pflt
          }
       }
 
@@ -165,10 +197,10 @@ object ProcRiechen extends Process {
    }
 
    private def perform( implicit tx: ProcTxn ) {
-      if( orgRef().isDefined ) {
-         inform( "can't perform - old performance ongoing!", force = true )
-         return // reentry??
-      }
+//      if( orgRef().isDefined ) {
+//         inform( "can't perform - old performance ongoing!", force = true )
+//         return // reentry??
+//      }
       inform( "perform" )
       startPlaying
       val pt = if( liveActive ) {
@@ -176,20 +208,25 @@ object ProcRiechen extends Process {
          if( rnd <= LIVE_PROB ) ReplaceLive else if( rnd - LIVE_PROB <= INT_PROB ) ReplaceInternal else ReplaceAll
       } else ReplaceInternal
       if( canReplaceTail( pt )) {
-         val pin  = factory( catchName ).make
-         val pout = factory( throwName ).make
-         val modChans: IIdxSeq[ Int ] = scramble2( 0 until MASTER_NUMCHANNELS )
-         val pflt = List.tabulate( MASTER_NUMCHANNELS ) { ch =>
-            val res = factory( urn.next ).make
-            res.control( "dur" ).v = exprand( MIN_DUR, MAX_DUR )
-            pin.audioOutput( "out" + ch ) ~> res
-            pin.audioOutput( "out" + modChans( ch )) ~> res.audioInput( "in2" )
-            res ~> pout.audioInput( "in" + ch )
-            res
-         }
-         val fdts = pflt.map( _ => exprand( MIN_FADEIN, MAX_FADEIN ))
-         orgRef.set( Some( Org( pin, pout, pflt.toSet )))
-         replaceTailChain( pin, pout, pflt, fadeTimes = Some( fdts ), point = pt )
+//         val pin  = factory( catchName ).make
+//         val pout = factory( throwName ).make
+//         val modChans: IIdxSeq[ Int ] = scramble2( 0 until MASTER_NUMCHANNELS )
+//         val pflt = List.tabulate( MASTER_NUMCHANNELS ) { ch =>
+//            val res = factory( urn.next ).make
+//            res.control( "dur" ).v = exprand( MIN_DUR, MAX_DUR )
+//            pin.audioOutput( "out" + ch ) ~> res
+//            pin.audioOutput( "out" + modChans( ch )) ~> res.audioInput( "in2" )
+//            res ~> pout.audioInput( "in" + ch )
+//            res
+//         }
+//         val fdts = pflt.map( _ => exprand( MIN_FADEIN, MAX_FADEIN ))
+//         orgRef.set( Some( Org( pin, pout, pflt.toSet )))
+//         replaceTailChain( pin, pout, pflt, fadeTimes = Some( fdts ), point = pt )
+         val p = factory( name ).make
+         replaceTail( p, point = pt )
+
+      } else {
+         reentry( 0.2 )
       }
    }
 }
